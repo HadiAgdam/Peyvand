9ae4174b5c6ecfc8b6b9514215a98545f860baa7
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-07-18 22:56:12 +0330

    added placeholder image

--- app/src/main/java/ir/hadiagdamapps/peyvand/MyApp.kt
+++ app/src/main/java/ir/hadiagdamapps/peyvand/MyApp.kt
@@ -2,10 +2,22 @@ package ir.hadiagdamapps.peyvand
 
 import android.app.Application
 import java.io.File
+import java.io.FileOutputStream
 
 class MyApp : Application() {
 
     private val cacheDirs = listOf("user")
+
+    private fun loadPlaceHolder() {
+        val f = File(cacheDir, "user/picture.png")
+        if (!f.exists())
+        {
+            val input = assets.open("picture_placeholder.png")
+            val output = FileOutputStream(f)
+
+            output.write(input.readBytes())
+        }
+    }
 
     private fun makCacheDirs() {
         for (dir in cacheDirs)
@@ -16,6 +28,7 @@ class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
         makCacheDirs()
+        loadPlaceHolder()
     }
 
 }
