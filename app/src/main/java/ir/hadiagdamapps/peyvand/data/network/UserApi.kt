7c94f650230eb91953e509a060ac7ee31e9e805c
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-12-20 23:50:12 +0330

    created UserApi

---
+++ app/src/main/java/ir/hadiagdamapps/peyvand/data/network/UserApi.kt
@@ -0,0 +1,44 @@
+package ir.hadiagdamapps.peyvand.data.network
+
+import com.android.volley.Request.Method
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import ir.hadiagdamapps.peyvand.data.models.KeySet
+import ir.hadiagdamapps.peyvand.data.models.PrivateKey
+import ir.hadiagdamapps.peyvand.data.models.PublicKey
+import ir.hadiagdamapps.peyvand.tools.Profile
+import org.json.JSONObject
+
+class UserApi(private val queue: RequestQueue) : Api() {
+
+    companion object {
+        private const val BASE_URL = "${Api.BASE_URL}/users"
+    }
+
+    fun register(
+        profile: Profile,
+        success: (KeySet) -> Unit,
+        failed: (ApiError?) -> Unit
+    ) {
+        queue.add(
+            JsonObjectRequest(Method.POST, BASE_URL, JSONObject().apply {
+
+                put("name", profile.name)
+                profile.picture?.urlString?.let { put("picture", it) }
+                put("bio", profile.bio.toString())
+
+            }, {
+
+                success(
+                    KeySet(
+                        public = PublicKey.parse(it.getString("public_key"))!!,
+                        private = PrivateKey.parse(it.getString("private_key"))!!
+                    )
+                )
+
+            }, { failed(it.toApiError()) })
+        )
+
+    }
+
+}
