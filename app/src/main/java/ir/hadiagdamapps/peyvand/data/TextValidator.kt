ed26d4edd51c5eb4ee6e64b64fc9829f70988fd8
HadiAgdam <hadiagdamdeveloper@gmail.com>
2025-01-31 15:34:48 +0330

    converted TextValidator to object

--- app/src/main/java/ir/hadiagdamapps/peyvand/data/TextValidator.kt
+++ app/src/main/java/ir/hadiagdamapps/peyvand/data/TextValidator.kt
@@ -2,49 +2,48 @@ package ir.hadiagdamapps.peyvand.data
 
 import ir.hadiagdamapps.peyvand.data.Key.*
 
-class TextValidator {
+object TextValidator {
 
-    companion object {
-        fun validateName(text: String): String? {
-            return text.ifBlank { null }
+    fun validateName(text: String): String? {
+        return text.ifBlank { null }
 
+    }
+
+    fun validateTel(text: String): String? {
+        return if (text.isNotBlank() &&
+            text.all { it.isDigit() } &&
+            text.length == 11 &&
+            text[0] == '0' &&
+            text[1] == '9'
+        ) text
+        else null
+    }
+
+    fun validateBio(text: String): String? {
+        return if (text.isNotBlank() && text.length <= 250) text
+        else null
+    }
+
+    fun isValidQrString(text: String): Boolean {
+
+        if (!text.startsWith(Constants.TARGET_URL))
+            return false
+
+        val args = text.removeRange(0, Constants.TARGET_URL.length + 1).split("&")
+
+        val headers = arrayListOf(NAME, PICTURE, TEL, BIO, PUBLIC_KEY)
+
+        args.forEach { arg ->
+            arg.split("=")[0].let { Key.fromString(it) }?.apply {
+
+                if (!headers.contains(this)) return false
+
+                headers.remove(this)
+            } ?: return false
         }
 
-        fun validateTel(text: String): String? {
-            return if (text.isNotBlank() &&
-                text.all { it.isDigit() } &&
-                text.length == 11 &&
-                text[0] == '0' &&
-                text[1] == '9'
-            ) text
-            else null
-        }
-
-        fun validateBio(text: String): String? {
-            return if (text.isNotBlank() && text.length <= 250) text
-            else null
-        }
-
-        fun isValidQrString(text: String): Boolean {
-
-            if (!text.startsWith(Constants.TARGET_URL))
-                return false
-
-            val args = text.removeRange(0, Constants.TARGET_URL.length + 1).split("&")
-
-            val headers = arrayListOf(NAME, PICTURE, TEL, BIO, PUBLIC_KEY)
-
-            args.forEach { arg ->
-                arg.split("=")[0].let { Key.fromString(it) }?.apply {
-
-                    if (!headers.contains(this)) return false
-
-                    headers.remove(this)
-                } ?: return false
-            }
-
-            return headers.size == 0
-        }
+        return headers.size == 0
+    }
 
     }
 }
