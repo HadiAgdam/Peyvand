bbc2c204099a7df406aa7659934a79ffb7c97ce8
HadiAgdam <hadiagdamdeveloper@gmail.com>
2025-02-03 16:40:44 +0330

    changed getStringOrNull to getNullString

--- app/src/main/java/ir/hadiagdamapps/peyvand/data/Extensions.kt
+++ app/src/main/java/ir/hadiagdamapps/peyvand/data/Extensions.kt
@@ -3,5 +3,5 @@ package ir.hadiagdamapps.peyvand.data
 import org.json.JSONObject
 
 
-fun JSONObject.getStringOrNull(key: String): String? = optString(key, null)
+fun JSONObject.getNullString(key: String): String? = optString(key, null)
 
